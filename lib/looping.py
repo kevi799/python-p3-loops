e33bff6825257b5a03799397ef67d406f73fd3d0
kelvinMulinge <kelvinmulinge9702@gmail.com>
2024-12-03 22:05:46 +0300

    working with loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,41 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    counter = 10
+
+    while counter > 0:
+        print(counter)
+        counter -= 1
+    print('Happy New Year!')
+happy_new_year()
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_integers = [num**2 for num in int_list]
+    
+    return squared_integers
+    
+square_integers([1, 2, 3, 4, 5])
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            i = 'FizzBuzz'
+            print(i)
+
+        elif i % 3 == 0:
+            i = 'Fizz'
+            print(i)
+            
+        elif i % 5 == 0:
+            i = 'Buzz'
+            print(i)
+        
+        else: 
+            print(i)
+           
+fizzbuzz()
+
+
+# fizzbuzz()
